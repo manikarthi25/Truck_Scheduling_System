79e5ef795a470aa244d2f32a99d4a253cb01a742
mani <manikarthi25@gmail.com>
2020-10-16 20:49:05 +0530

    Dc commit

---
+++ DistributedCenter/src/test/java/com/manikarthi2/DistributedCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manikarthi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
