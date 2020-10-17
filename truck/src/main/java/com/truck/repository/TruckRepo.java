43fcb3e0b971a55eb90440ddb01d499a59e0c9be
mani <manikarthi25@gmail.com>
2020-10-17 12:49:27 +0530

    commit

---
+++ truck/src/main/java/com/truck/repository/TruckRepo.java
@@ -0,0 +1,11 @@
+package com.truck.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.truck.entity.TruckEO;
+
+@Repository
+public interface TruckRepo extends JpaRepository<TruckEO, Integer> {
+
+}
